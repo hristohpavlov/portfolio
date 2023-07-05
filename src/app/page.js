537536374fd778cd25e22f0e8dfc31afb826abaa
hristohpavlov <hristohpavlov@gmail.com>
2023-07-06 02:57:22 +0300

    mvnbmnbv

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <div>
-          HRISTO PAVLOV
+          HRISTO PAVLOVmnbvmnbvmnbvmnbvmnbv
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
