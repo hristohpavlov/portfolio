f8af0d46e08c72176aeaee960955fa89f2183ca1
hristohpavlov <hristohpavlov@gmail.com>
2023-08-23 04:42:53 +0300

    Add Google Analytics

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -37,6 +37,21 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <AppContext.Provider value={{ ...state, dispatch }}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <ThemeProvider themeId={state.theme}>
         <LazyMotion features={domAnimation}>
           <Fragment>
