0ca25ed3378c145dae4a445c680491be54f6a095
hristohpavlov <hristohpavlov@gmail.com>
2023-08-31 18:50:41 +0300

    Fix Warnings in Console

--- src/layouts/Home/DisplacementSphere.js
+++ src/layouts/Home/DisplacementSphere.js
@@ -11,7 +11,7 @@ import {
   MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
-  SphereBufferGeometry,
+  SphereGeometry,
   UniformsUtils,
   Vector2,
   WebGLRenderer,
@@ -61,7 +61,7 @@ export const DisplacementSphere = props => {
     });
     renderer.current.setSize(innerWidth, innerHeight);
     renderer.current.setPixelRatio(1);
-    renderer.current.outputEncoding = SRGBColorSpace;
+    // renderer.current.outputEncoding = SRGBColorSpace;
 
     camera.current = new PerspectiveCamera(54, innerWidth / innerHeight, 0.1, 100);
     camera.current.position.z = 52;
@@ -81,7 +81,7 @@ export const DisplacementSphere = props => {
     };
 
     startTransition(() => {
-      geometry.current = new SphereBufferGeometry(32, 128, 128);
+      geometry.current = new SphereGeometry(32, 128, 128);
       sphere.current = new Mesh(geometry.current, material.current);
       sphere.current.position.z = 0;
       sphere.current.modifier = Math.random();
